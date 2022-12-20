e3d441f6afa55d15eedb3328d50177d20150a193
makyia bonner <makyiabonner@gmail.com>
2022-12-20 17:31:15 -0500

    Button Number inputs now fully functional

---
+++ index.js
@@ -0,0 +1,39 @@
+let number = document.querySelectorAll(".number")
+let sign = document.querySelectorAll(".sign");
+let display = document.querySelector(".display");
+let clear = document.querySelector(".clear");
+let equal = document.querySelector(".equal");
+
+//button number inputs
+number.forEach((number) => number.addEventListener('click', () => displayScreen(number.textContent)));
+sign.forEach((sign) => sign.addEventListener('click', () => displayScreen(sign.textContent)));
+clear.onclick = () => display.textContent = "";
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+function addition(a, b) {
+    return a + b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+let firstOperand = " ";
+let secondOperand = " ";
+
+function displayScreen(num){
+    display.textContent += num;
+    if(sign.onclick){
+        let firstOperand = display;
+        console.log(firstOperand);
+    }
+    console.log(display);
+}
+
