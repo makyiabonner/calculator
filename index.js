24b216ec936acf55593f6142b4a66e42f657e5e2
makyia bonner <makyiabonner@gmail.com>
2023-01-09 18:55:38 -0500

    Installing  ESLint

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
-let number = document.querySelectorAll(".number")
-let sign = document.querySelectorAll(".sign");
-let display= document.querySelector(".display");
-let clear = document.querySelector(".clear");
-let equal = document.querySelector(".equal");
-let dot = document.querySelector(".dot")
+const number = document.querySelectorAll('.number');
+const sign = document.querySelectorAll('.sign');
+const display = document.querySelector('.display');
+const clear = document.querySelector('.clear');
+const equal = document.querySelector('.equal');
+const dot = document.querySelector('.dot');
 
-//button number inputs
+// button number inputs
 number.forEach((number) => number.addEventListener('click', () => displayScreen(number.textContent)));
-dot.onclick = () => decimal(".");
-//Operator button inputs
+dot.onclick = () => decimal('.');
+// Operator button inputs
 sign.forEach((sign) => sign.addEventListener('click', () => operate(sign.textContent)));
 clear.onclick = () => clearAll();
-equal.onclick = () =>  display.textContent = calculate(operator,previousOperand, currentOperand);
+equal.onclick = () => display.textContent = calculate(operator, previousOperand, currentOperand);
 
-//Storage for First and Second Operand;
-let currentOperand = " ";
-let previousOperand = " ";
-let operator = " ";
+// Storage for First and Second Operand;
+let currentOperand = ' ';
+let previousOperand = ' ';
+let operator = ' ';
 let dotCount = 0;
 
-//Decimal button input
-function decimal(dots){
-    if(dots = "."){dotCount++;}
-    if(dotCount == 1){
-        currentOperand += ".";
-        display.textContent = currentOperand;
-    }
+// Decimal button input
+function decimal(dots) {
+  if (dots = '.') { dotCount++; }
+  if (dotCount == 1) {
+    currentOperand += '.';
+    display.textContent = currentOperand;
+  }
 }
 
-//Display Screen and Collecting Second Operand
-function displayScreen(num){
-    if(currentOperand.length <= 13){
+// Display Screen and Collecting Second Operand
+function displayScreen(num) {
+  if (currentOperand.length <= 13) {
     currentOperand += num;
-         display.textContent = currentOperand;
-    }
+    display.textContent = currentOperand;
+  }
 }
 
-//Making Operative buttons functional and Collecting First Operand
-function operate(op){
-    operator = op
-    if(operator == "+" || operator == "-" || operator == "*" || operator == "/"){
-        previousOperand = currentOperand;
-        currentOperand = " ";
-        dotCount = 0
-            }
-            }
+// Making Operative buttons functional and Collecting First Operand
+function operate(op) {
+  operator = op;
+  if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
+    previousOperand = currentOperand;
+    currentOperand = ' ';
+    dotCount = 0;
+  }
+}
 
 // Doing Math when Equal button pressed
-function calculate(operator,a,b){
-    a = Number(a);
-    b = Number(b);
-    switch(operator){
-        case "*":
-            return currentOperand = a * b; 
-        case "/":
-            return currentOperand = a / b;
-        case "+":
-            return currentOperand = a + b;
-        case "-":
-            return currentOperand = a - b;
-             }
+function calculate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case '*':
+      return currentOperand = a * b;
+    case '/':
+      return currentOperand = a / b;
+    case '+':
+      return currentOperand = a + b;
+    case '-':
+      return currentOperand = a - b;
+  }
 }
 
-//Clear button inputs
-function clearAll(){
-    previousOperand = "";
-    currentOperand = "";
-    dotCount = 0
-     display.textContent = "";
+// Clear button inputs
+function clearAll() {
+  previousOperand = '';
+  currentOperand = '';
+  dotCount = 0;
+  display.textContent = '';
 }
